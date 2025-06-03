665514a3d7931bb3e95eb909f02f7377fccb6d19
Mishra Aditya Ravi <a4920251m@gmail.com>
2025-06-03 15:52:37 +0530

    new starting

--- CH/function.py
+++
@@ -1,9 +0,0 @@
-#HI
-n = int(input("Enter a number you want factuorial of : "))
-def fec(n):
-    if n == 0 or n == 1:
-        return 1
-    else:
-        for i in range(1, n+1):
-            return n * fec(n-1)
-print(fec(n))
